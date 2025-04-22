c4fcfa44c770cef88f881a45c41a329a8fa6df76
OVillafan <ovillafan.ing@gmail.com>
2025-04-22 10:24:55 -0600

    Refactor AzureFunction

--- MetrobusWorkerService/MetrobusWorkerService/FunctionApp/MetrobusReceiverFunction.cs
+++ MetrobusWorkerService/MetrobusWorkerService/FunctionApp/MetrobusReceiverFunction.cs
@@ -1,24 +1,28 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-//using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using Azure.Storage.Blobs;
-using Microsoft.Azure.Functions.Worker;
 using System;
+using System.Net;
 
 namespace AzureFunctionMetrobus
 {
-    public static class MetrobusReceiverFunction
+    public class MetrobusReceiverFunction
     {
-        [FunctionName("SaveMetrobusData")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
-            ILogger log)
+        private readonly ILogger _logger;
+
+        public MetrobusReceiverFunction(ILoggerFactory loggerFactory)
         {
-            log.LogInformation("Recibiendo datos del Metrobus.");
+            _logger = loggerFactory.CreateLogger<MetrobusReceiverFunction>();
+        }
+
+        [Function("SaveMetrobusData")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequestData req)
+        {
+            _logger.LogInformation("Recibiendo datos del Metrobus.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
@@ -27,7 +31,6 @@ namespace AzureFunctionMetrobus
 
             // Leer el string de conexión del Storage desde configuración
             string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
-            // Aquí va
             string containerName = "raw-metrobus";
 
             // Guardar en Blob
@@ -37,7 +40,10 @@ namespace AzureFunctionMetrobus
             var ms = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(requestBody));
             await blob.UploadAsync(ms);
 
-            return new OkObjectResult($"Archivo guardado como {fileName}");
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteString($"Archivo guardado como {fileName}");
+
+            return response;
         }
     }
 }
